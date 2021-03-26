ab1214c0a2e3012a1cac38c690e41b0c541f1a5d
brunofrigeri <bpiraja97@gmail.com>
2021-03-26 15:14:28 -0300

    Stopped tracking .env File

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,11 +3,11 @@ import admin from 'firebase-admin'
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+      projectId: 'brunofrigeri-cbdf3',
       privateKey: process.env.FIREBASE_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
-    databaseURL: process.env.DATABASE_URL,
+    databaseURL: 'https://brunofrigeri-cbdf3-default-rtdb.firebaseio.com/',
   })
 }
 
