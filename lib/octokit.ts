bb8f7c11a26a481735524cbaec9a365005bea298
Bruno Frigeri <bpiraja97@gmail.com>
2022-12-21 09:31:38 -0300

    feat: testing github actions with octokit

---
+++ lib/octokit.ts
@@ -0,0 +1,7 @@
+import { Octokit } from '@octokit/core'
+
+const octokit = new Octokit({
+  auth: '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',
+})
+
+export default octokit
