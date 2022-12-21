bbf4be76e47224fab7c3c1abe8c953bf0689c1e9
Bruno Frigeri <bpiraja97@gmail.com>
2022-12-21 09:54:18 -0300

    feat: remove personal access key from branch

--- lib/octokit.ts
+++ lib/octokit.ts
@@ -1,7 +1,7 @@
 import { Octokit } from '@octokit/core'
 
 const octokit = new Octokit({
-  auth: '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',
+  auth: process.env.GITHUB_ACCESS_KEY,
 })
 
 export default octokit
