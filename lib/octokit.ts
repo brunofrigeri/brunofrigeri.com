40e2f592a5e6c0cf4407f006b2214832abedbc95
Bruno Frigeri <bpiraja97@gmail.com>
2022-12-28 23:04:42 -0300

    fix: fix octokit github access key vulnerability

--- lib/octokit.ts
+++ lib/octokit.ts
@@ -1,7 +1,7 @@
 import { Octokit } from '@octokit/core'
 
 const octokit = new Octokit({
-  auth: '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',
+  auth: process.env.GITHUB_ACCESS_KEY,
 })
 
 export default octokit
