0ac7fdd95807c6e037fb4eb0e36b2184d711ac04
Bruno Frigeri <bpiraja97@gmail.com>
2023-01-10 21:53:30 -0300

    feat: turns _document into a functional component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return initialProps
-  }
-
-  render() {
-    return (
-      <Html lang="en" className="font-custom">
-        <Head>
-          <link rel="icon" href="/assets/avatar.jpg" />
-          <meta
-            name="google-site-verification"
-            content="2H60FxwYAVnCGNBHtl_e4D3MqIXSDc8YRgjhpfjxczg"
-          />
-        </Head>
-        <body className="bg-white dark:bg-black">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en" className="font-custom">
+      <Head>
+        <link rel="icon" href="/assets/avatar.jpg" />
+        <meta
+          name="google-site-verification"
+          content="2H60FxwYAVnCGNBHtl_e4D3MqIXSDc8YRgjhpfjxczg"
+        />
+      </Head>
+      <body className="bg-white dark:bg-black">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
