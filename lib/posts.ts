2e1a66c0856b9e7da1bffa6be488c0af7bd1a3b8
Bruno Frigeri <bpiraja97@gmail.com>
2023-01-19 21:53:54 -0300

    feat: remove possibility to see post drafts

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,33 +1,33 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import readingTime from 'reading-time'
-import { format } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
-import en from 'date-fns/locale/en-US'
-import { Post, File } from './types'
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import readingTime from "reading-time";
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import en from "date-fns/locale/en-US";
+import { Post, File } from "./types";
 
-const postDirectory = path.join(process.cwd(), 'posts')
+const postDirectory = path.join(process.cwd(), "posts");
 
 export const formatData = (
   data: { [key: string]: any },
   content: string,
   slug: string
 ): Post => {
-  const readingData = readingTime(content)
+  const readingData = readingTime(content);
 
-  const isPtBrLocale = data.locale === 'pt-BR'
+  const isPtBrLocale = data.locale === "pt-BR";
 
   const reading = isPtBrLocale
     ? {
         ...readingData,
-        text: readingData.text.replace('read', 'de leitura'),
+        text: readingData.text.replace("read", "de leitura"),
       }
-    : readingData
+    : readingData;
 
-  const formattedDate = format(new Date(data.date), 'MMMM dd, yyyy', {
+  const formattedDate = format(new Date(data.date), "MMMM dd, yyyy", {
     locale: isPtBrLocale ? ptBR : en,
-  })
+  });
 
   return {
     ...data,
@@ -41,88 +41,88 @@ export const formatData = (
       excerpt: data.excerpt,
       date: formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1),
     },
-    featuredImage: data.featuredImage || '',
+    featuredImage: data.featuredImage || "",
     readingTime: reading,
-  }
-}
+  };
+};
 
 const readFile = (fileName: string) => {
-  const fullPath = path.join(postDirectory, fileName)
+  const fullPath = path.join(postDirectory, fileName);
 
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-  return fileContents
-}
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  return fileContents;
+};
 
 const getFileNames = (): File[] => {
   if (fs.existsSync(postDirectory)) {
-    const fileNames = fs.readdirSync(postDirectory)
+    const fileNames = fs.readdirSync(postDirectory);
 
     return fileNames.map((fileName) => {
-      const fileContents = readFile(fileName)
-      const { data } = matter(fileContents)
+      const fileContents = readFile(fileName);
+      const { data } = matter(fileContents);
 
-      return { fileName, locale: data.locale }
-    })
+      return { fileName, locale: data.locale };
+    });
   }
-}
+};
 
 export const getAllPosts = (locale: string): Post[] => {
   if (fs.existsSync(postDirectory)) {
-    const fileNames = getFileNames()
+    const fileNames = getFileNames();
 
-    if (fileNames.length === 0) return []
+    if (fileNames.length === 0) return [];
 
     const filteredData = fileNames
       .map(({ fileName }) => {
-        const slug = fileName.replace('.mdx', '')
+        const slug = fileName.replace(".mdx", "");
 
-        const fileContents = readFile(fileName)
-        const { data, content } = matter(fileContents)
-        const post = formatData(data, content, slug)
+        const fileContents = readFile(fileName);
+        const { data, content } = matter(fileContents);
+        const post = formatData(data, content, slug);
 
-        if (post.frontMatter.locale === locale) {
-          return post
+        if (!data?.draft && post.frontMatter.locale === locale) {
+          return post;
         }
       })
-      .filter(Boolean)
+      .filter(Boolean);
 
     return filteredData.sort((a, b) => {
       if (new Date(a.frontMatter.date) < new Date(b.frontMatter.date)) {
-        return 1
+        return 1;
       } else {
-        return -1
+        return -1;
       }
-    })
-  } else return []
-}
+    });
+  } else return [];
+};
 
 export const getAllPostSlugs = () => {
   if (fs.existsSync(postDirectory)) {
-    const fileNames = getFileNames()
+    const fileNames = getFileNames();
 
     const slugs = fileNames.map(({ fileName, locale }) => {
       return {
         params: {
-          slug: fileName.replace('.mdx', ''),
+          slug: fileName.replace(".mdx", ""),
         },
         locale,
-      }
-    })
+      };
+    });
 
-    return slugs
+    return slugs;
   } else {
-    return []
+    return [];
   }
-}
+};
 
 export const getPostBySlug = async (slug: string) => {
-  const fileContents = readFile(`${slug}.mdx`)
-  const { data, content } = matter(fileContents)
+  const fileContents = readFile(`${slug}.mdx`);
+  const { data, content } = matter(fileContents);
 
-  const post = formatData(data, content, slug)
+  const post = formatData(data, content, slug);
 
   return {
     slug,
     ...post,
-  }
-}
+  };
+};
